32f56d2d14e70b5c8bf3bbb9c6831c8e5e6763f6
Patrick Demichiel <patrick.demichiel@outlook.com>
2021-12-10 22:12:33 +0100

    icons added

--- src/VolumeScroller/Properties/Resources.Designer.cs
+++ src/VolumeScroller/Properties/Resources.Designer.cs
@@ -19,7 +19,7 @@ namespace VolumeScroller.Properties {
     // -Klasse über ein Tool wie ResGen oder Visual Studio automatisch generiert.
     // Um einen Member hinzuzufügen oder zu entfernen, bearbeiten Sie die .ResX-Datei und führen dann ResGen
     // mit der /str-Option erneut aus, oder Sie erstellen Ihr VS-Projekt neu.
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "17.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
     internal class Resources {
@@ -76,16 +76,6 @@ namespace VolumeScroller.Properties {
         internal static System.Drawing.Icon VolumeScroller_light {
             get {
                 object obj = ResourceManager.GetObject("VolumeScroller_light", resourceCulture);
-                return ((System.Drawing.Icon)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Icon ähnlich wie (Symbol).
-        /// </summary>
-        internal static System.Drawing.Icon VolumeScroller_light_ApplicationIcon {
-            get {
-                object obj = ResourceManager.GetObject("VolumeScroller_light_ApplicationIcon", resourceCulture);
                 return ((System.Drawing.Icon)(obj));
             }
         }
