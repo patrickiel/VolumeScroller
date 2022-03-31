63b39ddb6c5680d80b5c5f148b6c0ab42dbef09c
Patrick Demichiel <patrick.demichiel@outlook.com>
2022-03-31 17:59:00 +0200

    GlobalUsing added

---
+++ src/VolumeScroller/GlobalUsing.cs
@@ -0,0 +1,8 @@
+﻿global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using System.Diagnostics;
+global using System.ComponentModel;
+global using System.Windows;
--- src/VolumeScroller/MainModel.cs
+++ src/VolumeScroller/MainModel.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-
-namespace VolumeScroller;
+﻿namespace VolumeScroller;
 
 public class MainModel
 {
